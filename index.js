42780fde09673fed77eb219a2192a57de3286018
Kenan Yildirim <kenan@kenany.me>
2015-10-28 12:30:45 -0400

    refactor: use `overrustle-logs-url` module
    Closes https://github.com/KenanY/overrustle-logs/issues/6

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-var capitalize = require('capitalize');
 var hyperquest = require('hyperquest');
 var moment = require('moment');
 var split2 = require('split2');
-var join = require('url-join');
-
-// HTTPS actually supported
-var domain = 'https://overrustlelogs.net';
+var overrustleLogsUrl = require('overrustle-logs-url');
 
 function overrustle(opts) {
-  // begin with channel directory (first letter is capitalized)
-  var url = join(domain,
-    encodeURIComponent(capitalize(opts.channel) + ' chatlog'));
-
-  var date = moment.utc(opts.date);
-
-  // then go to a specific year-month
-  url = join(url,
-    encodeURIComponent(date.format('MMMM') + ' ' + date.format('YYYY')));
-
-  // get logs of specific user if desired
-  if (opts.user) {
-    url = join(url, 'userlogs', opts.user + '.txt');
-  }
-
-  // otherwise get logs of date
-  else {
-    url = join(url, date.format('YYYY-MM-DD') + '.txt');
-  }
-
   function parse(row) {
     // [2015-10-01 14:14:47 UTC] user: message
     return {
@@ -36,6 +12,8 @@ function overrustle(opts) {
       message: row.substring(row.indexOf(': ') + 2)
     };
   }
+
+  var url = overrustleLogsUrl(opts);
 
   return hyperquest(url).pipe(split2(parse));
 }
